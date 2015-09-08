f55dce672d449ee2e9b7c3fc67f2277b99925d84
ThuanNguyen <ducthuan.thaonguyenxanh31188@gmail.com>
2015-09-08 19:23:16 +0700

    - duc

---
+++ New Unity Project 1/Assets/Script/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+    static int numberObject = 3;
+    public GameObject[] dress = new GameObject[numberObject];
+    
+    int i = 0;
+    public void changeObjLeft()
+    {
+        Debug.Log("Test button");
+        dress[i].active = false;
+        i = (int)Mathf.Round((i - 1) % 3);
+        Debug.Log(i);
+        dress[i].active = true;
+    }
+    public void changeObjRight()
+    {
+        dress[i].active = false;
+        i = (int)Mathf.Round((i + 1) % 3);
+        Debug.Log(i);
+        dress[i].active = true;
+    }
+}
